04d675a157481ecd1b69cae9e42c4299461de435
superkingdan <463282453@qq.com>
2018-06-29 01:17:28 +0800

    demo

---
+++ springclouddemo2/eurekaserver/src/main/java/top/wangqichao/eurekaserver/EurekaserverApplication.java
@@ -0,0 +1,15 @@
+package top.wangqichao.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+//作为注册中心必带注解，否则启动无法看到注册中心
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaserverApplication.class, args);
+	}
+}
